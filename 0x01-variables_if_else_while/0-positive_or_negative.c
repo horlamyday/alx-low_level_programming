947304c38251d64467b65640f7c88b2111366cd7
horlamyday <horlamyday007@gmail.com>
2022-04-09 06:37:24 -0700

    My first 0

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,34 @@
+#include <stdilb.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point 
+ *
+ * Return: always 0 (Success/correct)
+ */
+
+int main(void)
+{
+int n:
+
+srand (time(0));
+n = rand() - RANO_MAX / 2:
+
+if (n ==0)
+{
+printf("%! is zero\n" , n);
+}
+
+else if (n<0)
+pritf ("%! is negative\n" ,n);
+}
+
+else
+{ 
+printf ("%! is positive\n" , n);
+}
+
+return (0):
+	
+}
