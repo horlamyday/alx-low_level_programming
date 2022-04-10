2cd089dafeb10953e4adcdc7b4cb155ae3f90b2b
horlamyday <horlamyday007@gmail.com>
2022-04-10 08:22:45 -0700

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entring point 
+ *
+ * Retrun: Always 0 (Success/correct)
+ */
+
+int main(void)
+
+{
+char alphabet;
+
+for (alphabet = 'a' ; alphabet <= 'z' ; alphaabet ++)
+{
+if  (alphabet == 'q')
+continue;
+
+else if (alphabet == 'e')
+continue;
+
+putchar (alphabet)
+}
+
+putchar ('\n');
+
+return (0);
+
+}
