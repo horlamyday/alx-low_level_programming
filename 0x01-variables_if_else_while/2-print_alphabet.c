22b247121cb2bb66fb771fa5b76d96d1558388bd
horlamyday <horlamyday007@gmail.com>
2022-04-10 06:44:28 -0700

    task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,11 +8,10 @@
  * Return: Always 0 (Success/currect)
  */
 int main(void)
-
 {
 char alphabet;
 
-for (alphabet = 'A'; alphabet  <= 'Z'; alphabet++)
+for(alphabet = 'A'; alphabet  <= 'Z'; alphabet++)
 {
 putchar(alphabat);
 }
