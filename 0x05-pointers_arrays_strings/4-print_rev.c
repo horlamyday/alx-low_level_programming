c068ed9fbd40faaf260879fee87f282ca7d0d01d
horlamyday <horlamyday007@gmail.com>
2022-04-20 17:39:58 -0700

    task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -3,16 +3,19 @@
  * * print_rev - prints a string in reverse
  * *@s: string to reverse
  * *Return: Nothing
- * */
+ */
 void print_rev(char *s)
 {
-		int l;
+int l;
 
-			l = 0;
-				while (*(s + l) != '\0')
-							l++;
-					l--;
-						for (; l >= 0; l--)
-									_putchar(*(s + l));
-							_putchar('\n');
+l = 0;
+while (*(s + l) != '\0')
+l++;
+l--;
+{
+for (; l >= 0; l--)
+
+_putchar(*(s + l));
+}
+_putchar('\n');
 }							
