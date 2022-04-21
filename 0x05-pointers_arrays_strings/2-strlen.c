4eea050289349ed8af50861832308b8724b8f8c3
horlamyday <horlamyday007@gmail.com>
2022-04-21 05:13:45 -0700

    task 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -6,10 +6,10 @@
  */
 int _strlen(char *s)
 {
-		int len;
+int len;
 
-			len = 0;
-				while (*(s + len) != '\0')
-							len++;
-					return (len);
+len = 0;
+while (*(s + len) != '\0');
+len++;
+return (len);
 }
