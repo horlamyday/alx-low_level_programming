5049ccbe711cb8a90e56790070576bf20a937273
horlamyday <horlamyday007@gmail.com>
2022-04-20 18:21:10 -0700

    task 100

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -5,26 +5,28 @@
  *   * @s: the string to be comverted.
  *    *
  *     *Return: The integer value of the comverted string.
- *      */
+ */
 
 int _atoi(char *s)
 {
 
-		int sign = 1;
-			unsigned int num = 0;
+int sign = 1;
+unsigned int num = 0;
 
-				do {
+do {
 
-							if (*s == '-')
-											sign *= -1;
+if (*s == '-')
+					
+sign *= -1;
 
-									else if (*s >= '0' && *s <= '9')
-													num = (num * 10) + (*s - '0');
+else if (*s >= '0' && *s <= '9')
+									
+num = (num * 10) + (*s - '0');
 
-											else if (num > 0)
-															break;
+else if (num > 0)
+break;
+}
+while (*s++);
 
-												} while (*s++);
-
-					return (num * sign);
+return (num * sign);
 }
