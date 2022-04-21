76f51807fe866598c69acf3bf7c49af9bd01fe6d
horlamyday <horlamyday007@gmail.com>
2022-04-20 18:32:49 -0700

    task 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -16,17 +16,17 @@ unsigned int num = 0;
 do {
 
 if (*s == '-')
-					
+
 sign *= -1;
 
 else if (*s >= '0' && *s <= '9')
-									
+
 num = (num * 10) + (*s - '0');
 
 else if (num > 0)
 break;
-}
-while (*s++);
 
-return (num * sign);
+}while (*s++);
+
+return (num *sign);
 }
