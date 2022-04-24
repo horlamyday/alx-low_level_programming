ba46ce7f078133de425162d16d9c753e87391f37
horlamyday <horlamyday007@gmail.com>
2022-04-24 10:50:14 -0700

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,21 +9,23 @@
 
 char *_strcat(char *dest, char *src)
 {
-      int i = 0;
-      int j = 0;
+	int i;
+	int j;
 
-      while (dest[i] != '\0')
-      {
-	    i++;
-      }
-      while (src[j] != '\0')
-      {
-             dest[i] = src[j];
-             j++;
-             i++;
-      }
+	i = 0;
+	j = 0;
 
-      dest[i] = '\0';
-      return (dest);
-}    
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
 
+	dest[i] = '\0';
+	return (dest);
+}   
